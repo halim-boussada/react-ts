8266c58b82cb5a73a260b333689f656ef3ff8837
halim-boussada <halimboussada10@gmail.com>
2026-03-04 09:08:57 +0100

    init

---
+++ src/Post.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+type Post = {
+  title: string;
+  body: string;
+  tags: string[];
+};
+function Posts() {
+  const [postList, setPostList] = useState<Post[]>([]);
+  async function getPosts() {
+    const {
+      data,
+    }: {
+      data: {
+        posts: Post[];
+      };
+    } = await axios.get("https://dummyjson.com/posts");
+    setPostList(data.posts);
+  }
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return (
+    <div>
+      <ul>
+        {postList.map((e: Post, index: number) => {
+          return (
+            <div key={index}>
+              <li>{e.title}</li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Posts;
